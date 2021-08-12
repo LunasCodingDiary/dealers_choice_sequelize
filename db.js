52cdba38dc5512cd75b02b77b18189697edd0db9
Luna <flunawonder@gmail.com>
2021-08-12 16:13:17 -0400

    0812done

--- db.js
+++ db.js
@@ -7,59 +7,168 @@ const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/dea
 //models 
 const Film = conn.define('film',{
  id:{
-
+   primaryKey: true,
+   type: UUID,
+   defaultValue: UUIDV4,
  },
  
- name:{
-
+ filmName:{
+    type: STRING, 
+    unique: true,
+    allowNull: false
  },
 
- categoryId:{
-
- }
+ // foreign key: categoryId
 })
 
 
 const Category = conn.define('category',{
     id:{
+      primaryKey: true,
+      type: UUID,
+      defaultValue: UUIDV4,
 
     },
-    name:{
-
+    categoryName:{
+     type: STRING, 
+     unique: true,
+     allowNull: true
     }
 })
 
 
 const User = conn.define('user',{
     id:{
+      primaryKey: true,
+      type: UUID,
+      defaultValue: UUIDV4,
 
     },
-    name:{
+    firstName:{
+      type: STRING, 
+      allowNull: false
 
     },
-    favCategory:{
-
-    }
+   // a question: how to generate favCategory by counting everything the person streamed?
 })
 
 const Streaming = conn.define('streaming',{
-
-
-
-
+    id: {
+        primaryKey: true,
+        type: UUID,
+        defaultValue: UUIDV4
+      },
+    time: {
+        type: DATE, 
+        allowNull: true,
+        defaultValue: ()=> new Date()
+        },
+    watchedById: {
+        type: UUID,
+        allowNull: true
+      },
+    filmId: {
+        type: UUID,
+        allowNull: false
+    }
 })
 
+//associations
+Category.hasMany(Film,{as:'category', foreignKey:'categoryId'}); //foreign key always in hasMany
+Film.belongsTo(Category)
 
+Streaming.belongsTo(User, {as:'streamedBy'});
+User.hasMany(Streaming,{foreignKey:'watchById'}); //so, if you write "hasMany", you can use "include[]"" in server, right?
+Streaming.belongsTo(Film);
+Film.hasMany(Streaming,{foreignKey:'filmId'});
+
+//datapoint
+const users = ['Monica', 'Sonia', 'Sophia', 'Walter', 'Daniel', 'Indigo', 'Luna', 'James', 'Jason', 'Andrea']
+;
+const films = ['Kingdom', 'Florida Project', 'Annette', 'Avatar', 'The Favourite', 'Lady Bird', 'Roma', 'Titanic', 'Ex Machina', 'The Master', 'Her', 'Chungking Express', 'Spirited Away', 'Only Yesterday', 'Waking Life','American Factory'];
+const categories= ['Horror', 'Sci-fi', 'Drama', 'Comedy', 'History', 'Asian', 'Independent', 'Documentary'];
 
 
 //sync and seed
-const syncAndSeed=async()=>{
+const syncAndSeed = async() =>{
     await conn.sync({force:true})
-    await Promise.all()
+    const [Monica, Sonia, Sophia, Walter, Daniel, Indigo, Luna, James, Jason, Andrea] = await Promise.all(users.map (firstName=> User.create({firstName})))
+    const [Horror, Scifi, Drama, Comedy, History, Asian, Independent, Documentary]= await Promise.all(categories.map (categoryName=> Category.create({categoryName})))
+    //Film & set its category
+    const [Kingdom, FloridaProject, Annette, Avatar, TheFavourite, LadyBird, Roma, Titanic, ExMachina, TheMaster, Her, ChungkingExpress, SpiritedAway, OnlyYesterday, WakingLife, AmericanFactory] = await Promise.all(films.map (filmName=> Category.create({filmName}))); //is this necessary?
+    
+    Kingdom.categoryId = Horror.id;  //question#1: is it okay that these variables are all capitalized?
+    Avatar.categoryId = Scifi.id;    //question#2: how can I add two categories to one film?? like a tag?
+    TheFavourite.categoryId = Drama.id;
+    FloridaProject.categoryId = Drama.id;
+    Annette.categoryId = Scifi.id;
+    LadyBird.categoryId = Independent.id;
+    Roma.categoryId = History.id;
+    Titanic.categoryId = History.id;
+    ExMachina.categoryId = Scifi.id;
+    TheMaster.categoryId = History.id;
+    Her.categoryId = Scifi.id;
+    ChungkingExpress.categoryId = Comedy.id;
+    SpiritedAway.categoryId = Asian.id;
+    OnlyYesterday.categoryId = Asian.id;
+    WakingLife.categoryId = Scifi.id;
+    AmericanFactory.categoryId = Documentary.id;
 
+    await Promise.all([Kingdom.save(), Avatar.save(), TheFavourite.save(),FloridaProject.save(), Annette.save(), LadyBird.save(), Roma.save(), Titanic.save(), ExMachina.save(), TheMaster.save(), Her.save(), ChungkingExpress.save(), SpiritedAway.save(), OnlyYesterday.save(), WakingLife.save(), AmericanFactory.save()]);
+    //question#3: is there an easier way to save everything here?  
 
+    //create some Streaminng history
+    await Promise.all([
+            Streaming.create({
+              streamedById: Sophia.id,
+              filmId: Kingdom.id
+            }),
+            Streaming.create({
+                streamedById: Sonia.id,
+                filmId: Avatar.id
+              }),
+            Streaming.create({
+                streamedById: Indigo.id,
+                filmId: LadyBird.id
+            }),
+            Streaming.create({
+                streamedById: Sophia.id,
+                filmId: Kingdom.id
+            }),
+            Streaming.create({
+                streamedById:Jason.id,
+                filmId: Her.id
+              }),
+            Streaming.create({
+                streamedById: Sophia.id,
+                filmId: Annette.id
+            }),
+            Streaming.create({
+                streamedById: Monica.id,
+                filmId: ExMachina.id
+            }),
+            Streaming.create({
+                streamedById: Luna.id,
+                filmId: ChungkingExpress.id
+            }),
+            Streaming.create({
+                streamedById: Walter.id,
+                filmId: SpiritedAway.id
+            }),
+            Streaming.create({
+                streamedById: Andrea.id,
+                filmId: ChungkingExpress.id
+            }),
+            Streaming.create({
+                streamedById: James.id,
+                filmId: AmericanFactory.id
+            }),
+            Streaming.create({
+                streamedById: Daniel.id,
+                filmId: LadyBird.id
+            })
+            ]);
 }
-
 
 //exports 
 module.exports = {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //require db, express, and other middlewares(eg. morgan, method override)
 const db = require ('./db.js');
+const{models:{Film, Category, User, Streaming}} = db //deconstrction
 const express = require ('express');
 const morgan = require('morgan')
 
@@ -10,52 +11,59 @@ app.use(express.urlencoded({extended:false}))
 app.use(morgan)
 
 //app.get(routing)
-app.get('/', async(req, res, next){
+app.get('/',(req, res, next)=> res.redirect('/api/films'))
+
+app.get('/api/films', async(req, res, next)=>{
     try{
-        
+        res.send(await Film.findAll({
+            include:[Streaming]
+        }));
     } catch(error){
         next(error)
     }
 })
 
-app.get('/', async(req, res, next){
+app.get('/api/users', async(req, res, next)=>{
     try{
-
+        res.send(await User.findAll({
+            include:[Streaming]
+        }));
     } catch(error){
         next(error)
     }
 })
 
-app.get('/', async(req, res, next){
+app.get('/categories', async(req, res, next)=>{
     try{
-
+        res.send(await Category.findAll({
+            include:[Film]
+        }));
     } catch(error){
         next(error)
     }
 })
 
-app.get('/', async(req, res, next){
-    try{
-
-    } catch(error){
-        next(error)
-    }
-})
-
-app.get('/', async(req, res, next){
-    try{
-
+app.get('/streaming', async(req, res, next)=>{
+    try{ 
+        res.send(await Streaming.findAll({
+        include:[Film,
+        {model: User, as:'streamedBy'}]
+    }));
     } catch(error){
         next(error)
     }
 })
 
 //init : syncAndSeed + setup the Port 
-const init = async() => {
+const setUp = async() => {
+    try{
     await db.syncAndSeed();
+    const port = process.env.PORT || 3010 ; 
+    app.listen(port,()=>console.log('app is listening to Port 3010'));
+    }catch(error){
+        console.log(error)
+    }
+};
+//question: what's the difference between setUp and init? Why do we use try-ctach here as well?
 
-    const port = process.env.PORT || 0810 ; 
-    app.listen(port,()=>console.log('app is listening to Port 0810'));
-}
-
-init()
+setUp()
